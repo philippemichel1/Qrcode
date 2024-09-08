f3301ee46aa076b8b046666a7af9844c47989423
Philippe michel <philippe.michel1@gmail.com>
2024-09-08 16:12:27 +0200

    Initial Commit

---
+++ QRcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRcode
+//
+//  Created by Philippe MICHEL on 08/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRcodeTests/QRcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodeTests.swift
+//  QRcodeTests
+//
+//  Created by Philippe MICHEL on 08/09/2024.
+//
+
+import Testing
+@testable import QRcode
+
+struct QRcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
