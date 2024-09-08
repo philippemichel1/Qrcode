b80c0a604f99a0b51927a5e5332ad7793c789b58
Philippe michel <philippe.michel1@gmail.com>
2024-09-08 17:23:35 +0200

    Début de projet

--- QRcode/ContentView.swift
+++ QRcode/ContentView.swift
@@ -6,19 +6,70 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var text = "www.example.com"
+    @State private var qrCodeImage: UIImage?
+    @State private var isSheetPresented = false
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter text", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button(action: {
+                qrCodeImage = generateQRCode(from: text)
+                isSheetPresented = true
+            }) {
+                Text("Générer le QR Code")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
         }
         .padding()
+        .sheet(isPresented: $isSheetPresented) {
+            if let qrCodeImage = qrCodeImage {
+                QRCodeSheetView(qrCodeImage: qrCodeImage)
+            }
+        }
+    }
+
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        return nil
     }
 }
 
+struct QRCodeSheetView: View {
+    let qrCodeImage: UIImage
+
+    var body: some View {
+        VStack {
+            Image(uiImage: qrCodeImage)
+                .interpolation(.none)
+                .resizable()
+                .frame(width: 200, height: 200)
+                .padding()
+
+            Spacer()
+        }
+        .presentationDetents([.medium]) // Affiche la feuille à demi-hauteur
+    }
+}
 #Preview {
     ContentView()
 }
