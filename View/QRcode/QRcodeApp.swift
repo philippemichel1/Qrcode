72a6fa6e43d69f683b877bb6a5c6633b84726a56
Philippe michel <philippe.michel1@gmail.com>
2024-09-09 19:03:26 +0200

    reorganisation du projet

---
+++ View/QRcode/QRcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodeApp.swift
+//  QRcode
+//
+//  Created by Philippe MICHEL on 08/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct QRcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
