3a6880781a81828b9f72be678bce802a53b348e9
Philippe michel <philippe.michel1@gmail.com>
2024-09-09 18:50:58 +0200

    Export de vue d'affichge du qrcode

---
+++ QRcode/QRCodeSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  QRCodeSheetView.swift
+//  QRcode
+//
+//  Created by Philippe MICHEL on 09/09/2024.
+//
+
+import SwiftUI
+
+struct QRCodeSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRCodeSheetView()
+}
